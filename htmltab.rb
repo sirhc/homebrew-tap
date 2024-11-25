ec3e19ec3e39a1052de10828ee7f0ab169179854
Chris Grau <113591+sirhc@users.noreply.github.com>
2024-11-25 12:42:51 -0800

    Add formula for htmltab

---
+++ htmltab.rb
@@ -0,0 +1,115 @@
+class Htmltab < Formula
+  include Language::Python::Virtualenv
+
+  desc "Command-line utility to convert HTML tables into CSV files"
+  homepage "https://flother.github.io/htmltab/"
+  url "https://github.com/flother/htmltab/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "8db2a2960719ec5ea77b8f8e62b64732782a7e81f169664b161ca258ad7ec161"
+  license "MIT"
+  revision 1
+  head "https://github.com/flother/htmltab.git", branch: "master"
+
+  depends_on "cython" => :build
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.10"
+
+  resource "lxml" do # ~= 4.7
+    url "https://files.pythonhosted.org/packages/84/74/4a97db45381316cd6e7d4b1eb707d7f60d38cb2985b5dfd7251a340404da/lxml-4.7.1.tar.gz"
+    sha256 "a1613838aa6b89af4ba10a0f3a972836128801ed008078f8c1244e65958f1b24"
+  end
+
+  resource "soupsieve" do # > 1.2
+    url "https://files.pythonhosted.org/packages/d7/ce/fbaeed4f9fb8b2daa961f90591662df6a86c1abf25c548329a86920aedfb/soupsieve-2.6.tar.gz"
+    sha256 "e2e68417777af359ec65daac1057404a3c8a5455bb8abc36f1a9866ab1a51abb"
+  end
+
+  resource "six" do # >= 1.9
+    url "https://files.pythonhosted.org/packages/71/39/171f1c67cd00715f190ba0b100d606d440a28c93c7714febeca8b79af85e/six-1.16.0.tar.gz"
+    sha256 "1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926"
+  end
+
+  resource "webencodings" do
+    url "https://files.pythonhosted.org/packages/0b/02/ae6ceac1baeda530866a85075641cec12989bd8d31af6d5ab4a3e8c92f47/webencodings-0.5.1.tar.gz"
+    sha256 "b36a1c245f2d304965eb4e0a82848379241dc04b865afcc4aab16748587e1923"
+  end
+
+  resource "html5lib" do
+    url "https://files.pythonhosted.org/packages/ac/b6/b55c3f49042f1df3dcd422b7f224f939892ee94f22abcf503a9b7339eaf2/html5lib-1.1.tar.gz"
+    sha256 "b2e5b40261e20f354d198eae92afc10d750afb487ed5e50f9c4eaf07c184146f"
+  end
+
+  resource "beautifulsoup4" do # ~= 4.10
+    url "https://files.pythonhosted.org/packages/a1/69/daeee6d8f22c997e522cdbeb59641c4d31ab120aba0f2c799500f7456b7e/beautifulsoup4-4.10.0.tar.gz"
+    sha256 "c23ad23c521d818955a4151a67d81580319d4bf548d3d49f4223ae041ff98891"
+  end
+
+  resource "click" do # ~= 8.0
+    url "https://files.pythonhosted.org/packages/dd/cf/706c1ad49ab26abed0b77a2f867984c1341ed7387b8030a6aa914e2942a0/click-8.0.4.tar.gz"
+    sha256 "8458d7b1287c5fb128c90e23381cf99dcde74beaf6c7ff6384ce84d6fe090adb"
+  end
+
+  resource "cssselect" do # ~= 1.1
+    url "https://files.pythonhosted.org/packages/70/54/37630f6eb2c214cdee2ae56b7287394c8aa2f3bafb8b4eb8c3791aae7a14/cssselect-1.1.0.tar.gz"
+    sha256 "f95f8dedd925fd8f54edb3d2dfb44c190d9d18512377d3c1e2388d16126879bc"
+  end
+
+  resource "charset_normalizer" do # ~= 2.0.0
+    url "https://files.pythonhosted.org/packages/56/31/7bcaf657fafb3c6db8c787a865434290b726653c912085fbd371e9b92e1c/charset-normalizer-2.0.12.tar.gz"
+    sha256 "2857e29ff0d34db842cd7ca3230549d1a697f96ee6d3fb071cfa6c7393832597"
+  end
+
+  resource "flit_core " do # >= 3.2, < 4
+    url "https://files.pythonhosted.org/packages/d5/ae/09427bea9227a33ec834ed5461432752fd5d02b14f93dd68406c91684622/flit_core-3.10.1.tar.gz"
+    sha256 "66e5b87874a0d6e39691f0e22f09306736b633548670ad3c09ec9db03c5662f7"
+  end
+
+  resource "idna" do # >= 2.5, < 4
+    url "https://files.pythonhosted.org/packages/f1/70/7703c29685631f5a7590aa73f1f1d3fa9a380e654b86af429e0934a32f7d/idna-3.10.tar.gz"
+    sha256 "12f65c9b470abda6dc35cf8e63cc574b1c52b11df2c86030af0ac09b01b13ea9"
+  end
+
+  resource "urllib3" do # >= 1.21.1, < 1.27
+    url "https://files.pythonhosted.org/packages/e4/e8/6ff5e6bc22095cfc59b6ea711b687e2b7ed4bdb373f7eeec370a97d7392f/urllib3-1.26.20.tar.gz"
+    sha256 "40c2dc0c681e47eb8f90e7e27bf6ff7df2e677421fd46756da1161c39ca70d32"
+  end
+
+  resource "certifi" do # >= 2017.4.17
+    url "https://files.pythonhosted.org/packages/b0/ee/9b19140fe824b367c04c5e1b369942dd754c4c5462d5674002f75c4dedc1/certifi-2024.8.30.tar.gz"
+    sha256 "bec941d2aa8195e248a60b31ff9f0558284cf01a52591ceda73ea9afffd69fd9"
+  end
+
+  resource "requests" do # ~= 2.27
+    url "https://files.pythonhosted.org/packages/60/f3/26ff3767f099b73e0efa138a9998da67890793bfa475d8278f84a30fec77/requests-2.27.1.tar.gz"
+    sha256 "68d7c56fd5a8999887728ef304a6d12edc7be74f1cfa47714fc8b414525c9a61"
+  end
+
+  patch :DATA
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_predicate bin/"htmltab", :executable?
+    assert_match "htmltab, version 0.2.0", shell_output("#{bin}/htmltab --version")
+  end
+end
+
+__END__
+--- setup.py.orig	2024-11-25 12:30:14.967017167 -0800
++++ setup.py	2024-11-25 12:30:33.194722729 -0800
+@@ -3,7 +3,7 @@
+ 
+ setup(
+     name="htmltab",
+-    use_scm_version=True,
++    version="0.2.0",
+     description="A command-line utility that converts an HTML table into CSV data",
+     long_description=open("README.md").read(),
+     url="https://github.com/flother/htmltab",
+@@ -35,5 +35,4 @@
+         "pytest-flake8 ~= 1.0",
+         "httmock ~= 1.4",
+     ],
+-    setup_requires=["setuptools_scm", "pytest-runner"],
+ )
